ff7becbad127516b4c7959f3cede635c48cb5b52
Trey Turner <42526038+TrNiTu@users.noreply.github.com>
2018-09-05 18:47:25 -0500

    Starting it up
    Created the header, summary, and main of the program

---
+++ refraction.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Trey Turner
+ *
+ *
+ * This is a program that prompts inputs of
+ * an angle of incidence and an index of refraction
+ * and computes then prints the angle of refraction
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char **argv){
+
+  return 0;
+}
