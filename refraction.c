b6ecdfbca9432bd255b50026bbc9ca6429e2bf5c
Trey Turner <42526038+TrNiTu@users.noreply.github.com>
2018-09-05 19:21:29 -0500

    Nearing Completion
    Getting completely different values than the expected output. Has to do with the equation. Finding possible solution.

--- refraction.c
+++ refraction.c
@@ -13,5 +13,23 @@
 
 int main(int argc, char **argv){
 
+  double indexOfRefraction, angleOfRefraction, angleOfIncidence;
+  printf("Please enter an angle of incidence value.\n");
+  scanf("%lf",&angleOfIncidence);
+  if(angleOfIncidence>360||angleOfIncidence<0){
+    printf("You've entered an invalid value (%lf). Exiting program...", angleOfIncidence);
+    exit(1);
+  }
+  printf("Please enter an index of refraction value.\n");
+  scanf("%lf", &indexOfRefraction);
+  if(indexOfRefraction>5||indexOfRefraction<1){
+    printf("You've entered an invalid value (%lf). Exiting program...", indexOfRefraction);
+    exit(1);
+  }
+  angleOfRefraction = asin(sin(angleOfIncidence)/indexOfRefraction);
+  printf("Angle of Incidence: %.0f degrees\n", angleOfIncidence);
+  printf("Index of Refraction: %.3lf\n", indexOfRefraction);
+  printf("Angle of Refraction: %.2lf\n", angleOfRefraction);
+
   return 0;
 }
